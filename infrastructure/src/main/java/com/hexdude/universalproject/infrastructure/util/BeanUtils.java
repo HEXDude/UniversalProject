889c5b266a25f1d75a6afb510f889204520ef686
HEXDude <Development.HEXDude@Outlook.com>
2023-01-21 17:39:48 +0800

    :sparkles: Utils.

--- infrastructure/src/main/java/com/hexdude/universalproject/infrastructure/util/BeanUtils.java
+++ infrastructure/src/main/java/com/hexdude/universalproject/infrastructure/util/BeanUtils.java
@@ -7,4 +7,16 @@ package com.hexdude.universalproject.infrastructure.util;
  */
 public class BeanUtils {
 
+    /**
+     * 对象深拷贝
+     *
+     * @param source 源对象
+     * @param target 目标对象
+     * @return 目标对象
+     * @author HEXDude
+     * @date 2023-01-21
+     */
+    public static <S, T> T deepCopy(S source, T target) {
+        return target;
+    }
 }
